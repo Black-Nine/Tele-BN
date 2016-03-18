3295f5f113cb9dd4469c687823feba1f3437229a
TELE BN <neorabbit.m@gmail.com>
2016-03-18 12:57:39 +0330

    Update bot.lua
    telebn

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {151442858,116251654},
+    sudo_users = {151442858},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
